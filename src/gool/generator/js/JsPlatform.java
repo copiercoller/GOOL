f46c1ebfc8e1efe14c1acb96b7583e1be63df45f
copiercoller <bonjour@laposte.net>
2014-01-21 13:49:31 +0100

    ajout des fichiers nécessaire a la jgeneration du js (fonctionnel) todo: creer le code spécifique au js ( actuellement génere un fichier.js contenant du java)

---
+++ src/gool/generator/js/JsPlatform.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2010 Pablo Arrighi, Alex Concha, Miguel Lezama for version 1.
+ * Copyright 2013 Pablo Arrighi, Miguel Lezama, Kevin Mazet for version 2.    
+ *
+ * This file is part of GOOL.
+ *
+ * GOOL is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation, version 3.
+ *
+ * GOOL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License version 3 for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with GOOL,
+ * in the file COPYING.txt.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package gool.generator.js;
+
+import gool.Settings;
+import gool.executor.common.SpecificCompiler;
+import gool.executor.java.JavaCompiler;
+import gool.generator.common.CodePrinter;
+import gool.generator.common.Platform;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * This is the default Java Platform. It uses the output folders specified in
+ * Settings. It uses JavaCodePrinter for concrete Java generation. It uses
+ * JavaCompiler for compiling and executing the generated concrete Java. Since
+ * it is fully specified, and since there should only ever be one instance of
+ * it, this could almost have been a static class. But since platforms in
+ * general are objects, this is not the case. Still, we make sure that there is
+ * always only one instance of this object.
+ */
+public final class JsPlatform extends Platform {
+
+	private final String outputDir = Settings.get("js_out_dir");
+
+	private JsPlatform(Collection<File> myFile) {
+		super("JS", myFile);
+	}
+
+	@Override
+	protected CodePrinter initializeCodeWriter() {
+		return new JsCodePrinter(new File(outputDir), myFileToCopy);
+	}
+
+	@Override
+	protected SpecificCompiler initializeCompiler() {
+		return new JavaCompiler(new File(outputDir), new ArrayList<File>());
+	}
+
+	/**
+	 * What follows is to make sure that we always use the same instance of this
+	 * object.
+	 */
+
+	private static JsPlatform instance = new JsPlatform(myFileToCopy);
+
+	public static JsPlatform getInstance(Collection<File> myF) {
+		myFileToCopy = myF;
+		return instance;
+	}
+
+	public static JsPlatform getInstance() {
+		if (myFileToCopy == null) {
+			myFileToCopy = new ArrayList<File>();
+		}
+		return instance;
+	}
+
+	public static void newInstance() {
+		instance = new JsPlatform(myFileToCopy);
+	}
+}
